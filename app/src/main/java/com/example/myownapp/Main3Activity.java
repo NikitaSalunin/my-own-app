0b33826327d864723cd78e53ac43ffe8a6b333ff
NikitaSa250 <himikho270@gmail.com>
2020-05-24 16:15:58 +0300

    app

--- app/src/main/java/com/example/myownapp/Main3Activity.java
+++ app/src/main/java/com/example/myownapp/Main3Activity.java
@@ -9,20 +9,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
-import android.widget.TextView;
 
-import java.util.Random;
-
-public class Main2Activity extends AppCompatActivity {
+public class Main3Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main3);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        final Random random = new Random();
-   
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -34,20 +29,4 @@ public class Main2Activity extends AppCompatActivity {
         });
     }
 
-    public static class Main
-    {
-        public static int getRandomDiceNumber()
-        {
-            return (int) (Math.random() * 6);
-        }
-        public static void main(String[] args)
-        {
-            for (int i = 0; i < 10; i++)
-            {
-                int x = getRandomDiceNumber();
-
-                System.out.println(x);
-            }
-        }
-    }
 }
--- app/src/main/java/com/example/myownapp/MainActivity.java
+++ app/src/main/java/com/example/myownapp/MainActivity.java
@@ -17,44 +17,68 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-private EditText pass;
-private Button button;
-private RatingBar rating;
-private TextView text_show;
+    private Button button;
+    private Button button2;
+    private Button button3;
+    private Button button4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         addListenerOnButton();
+        addListenerOnButton2();
+        addListenerOnButton3();
+        addListenerOnButton4();
     }
 
+
     public void addListenerOnButton() {
-        button= (Button) findViewById(R.id.button);
+        button = (Button) findViewById(R.id.button);
         button.setOnClickListener(
-        new View.OnClickListener() {
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
+                        startActivity(intent);
+                    }
 
-            @Override
-            public void onClick(View v) {
-                button.setText("Done");
-                Toast.makeText(
-                        MainActivity.this, pass.getText(),
-                        Toast.LENGTH_SHORT
-                ).show();
-            }
+                });
+    }
+    public void addListenerOnButton2() {
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(getApplicationContext(), Main3Activity.class);
+                        startActivity(intent);
+                    }
 
-
-        });
-button.setOnClickListener(
-        new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(".Main2Activity");
-startActivity(intent);
-
-
-        }
-
-});
+                });
 }
+    public void addListenerOnButton3() {
+        button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(getApplicationContext(), Main4Activity.class);
+                        startActivity(intent);
+                    }
+
+                });
+    }
+    public void addListenerOnButton4() {
+        button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(getApplicationContext(), Main5Activity.class);
+                        startActivity(intent);
+                    }
+
+                });
+    }
 }
